c5b534a5bafad2d72952b56b92cde203fa1e4864
Nikhil Samant <nikhilsamant4@hotmail.com>
2020-10-24 00:23:26 +0530

    SQL Script updated

--- Configurator.Web/Database.sql
+++ Configurator.Web/Database.sql
@@ -3,5 +3,9 @@ Create Database Configurator;
 Use Configurator;
 
 Create table Configuration (
-    Id INT
+    Id INT IDENTITY(1,1),
+    ConfigurationName NVARCHAR(50),
+    ConfigType NVARCHAR(20),
+    Config NVARCHAR(MAX),
+    CONSTRAINT PK_Configuration PRIMARY KEY (Id)
 )
